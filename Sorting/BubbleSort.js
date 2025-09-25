d918b33cefb32643978b7404f392eaa0aad01a12
swethabpalanisamy <swethapalanisamy18@gmail.com>
2025-09-25 19:48:53 +0530

    Buuble Sort code

---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ */
+class Solution {
+    bubbleSort(arr) {
+        // code here
+        for (let i = 0; i < arr.length; i++) {
+            let swapped = false;
+            for (let j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+
+                }
+            }
+            if (!swapped) break;
+        }
+        return arr;
+    }
+}
+let arr = [4, 1, 3, 9, 7];
+const bubbleSortClass = new Solution();
+console.log(bubbleSortClass.bubbleSort(arr));
